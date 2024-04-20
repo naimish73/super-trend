3b97cf4b285ddfaf7217b62c41e36019a8062aeb
Naimish Bhuva <naimishbbhuva@gmail.com>
2024-04-20 10:36:56 +0530

    ci/cd: some changes

--- super_trend.py
+++ super_trend.py
@@ -16,25 +16,39 @@ def fetch_live_data(symbol):
     global has_fetched_full_day_data, symbol_data
     if has_fetched_full_day_data:
         # Fetch the full day's data and select the last row
-        latest_data = Ticker(symbol).history(interval="1d", start="2023-01-01", end="2024-02-19")
+        latest_data = Ticker(symbol).history(interval="1m", period='1m')
         latest_data = latest_data.iloc[-1:]
         # print('latest_data:', latest_data)
         # Concatenate the latest minute's data with the existing DataFrame
         symbol_data = pd.concat([symbol_data, latest_data])
     else:
         # Fetch the full day's data
-        symbol_data = Ticker(symbol).history(interval="1d", start="2023-01-01", end="2024-02-19")
+        symbol_data = Ticker(symbol).history(interval="1m", period='1d')
         has_fetched_full_day_data = True
 
     # print('\n\nlive data:\n', symbol_data)
+    symbol_data.to_csv('dataset/symbol_data.csv')
     return symbol_data
 
 # Function to calculate the SuperTrend
 def calculate_supertrend(data):
-    super_trend = ta.supertrend(data['High'], data['Low'], data['Close'], atr=20, multiplier=2)
+    atrPeriod = 20  # ATR Length
+    factor = 2.0    # Factor
+
+    # Calculate SuperTrend
+    # super_trend, direction = ta.supertrend(data['High'], data['Low'], data['Close'], atrPeriod, factor)
+    super_trend = ta.supertrend(data['High'], data['Low'], data['Close'], atrPeriod, factor)
+    print('super_trend:', super_trend)
+
+    # Adjust for backtesting purposes
+    super_trend = super_trend.shift(1)
+
+    # Save the SuperTrend values to a CSV file
+    # super_trend_df = pd.DataFrame({'SuperTrend': super_trend, 'Direction': direction}, index=data.index)
     super_trend.to_csv('dataset/super_trend.csv')
-    # print('\n\nSuper Trend:\n', super_trend)
+
     return super_trend
+
 
 # Function to plot the SuperTrend and symbol data together
 def plot_supertrend_with_symbol_data(super_trend, symbol_data):
@@ -43,8 +57,8 @@ def plot_supertrend_with_symbol_data(super_trend, symbol_data):
     # for index, row in super_trend.iterrows():
     #     print(index, row['SUPERTd_7_2.0'])
 
-    ax1.plot(super_trend.index, super_trend['SUPERTl_7_2.0'], label="Supertrend", color='green')
-    ax1.plot(super_trend.index, super_trend['SUPERTs_7_2.0'], label="Supertrend", color='red')
+    ax1.plot(super_trend.index, super_trend['SUPERTl_20_2.0'], label="Supertrend", color='green')
+    ax1.plot(super_trend.index, super_trend['SUPERTs_20_2.0'], label="Supertrend", color='red')
 
     ax1.set_ylabel('Supertrend', color='blue')
     ax1.tick_params('y', colors='blue')
@@ -58,16 +72,16 @@ def plot_supertrend_with_symbol_data(super_trend, symbol_data):
     fig.tight_layout()
     plt.ion()
     plt.legend()
-    plt.show()
-    plt.pause(60)
-    plt.close()
+    plt.show(block=True)
+    # plt.pause()
+    # plt.close()
 
 # Main function to start the live plotting
 def main(symbol):
-    while True:
-        data = fetch_live_data(symbol)
-        super_trend = calculate_supertrend(data)
-        plot_supertrend_with_symbol_data(super_trend, data)
+    # while True:
+    data = fetch_live_data(symbol)
+    super_trend = calculate_supertrend(data)
+    plot_supertrend_with_symbol_data(super_trend, data)
 
 # Start the live plotting
 main(symbol)
